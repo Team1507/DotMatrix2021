b6e6e27d62881bbe065051d97ce282b77a7649de
Benjamin LaGreca <btl201@rit.edu>
2021-06-30 19:36:09 -0400

    death incarnate

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,181 +1,47 @@
-#include "Robot.h"
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc2/command/CommandScheduler.h>
+#include <frc/drive/DifferentialDrive.h>
+#include "ctre/Phoenix.h"
 
-void Robot::RobotInit()
- {
-    //*************************** INIT ******************************
-    std::cout<<"RobotInit"<<std::endl;
-    std::cout<<"FRC2021: Infinite Recharge code port"<<std::endl;
-    std::cout<<"          ~~~ DotMatrix ~~~"<<std::endl;
-    std::cout<<"Version: " << __DATE__ <<"  "<<__TIME__<<std::endl<<std::endl; 
+#include <frc/TimedRobot.h>
+#include <frc2/command/button/JoystickButton.h>
 
-  //Julia was here
+#include <frc2/command/Command.h>
 
-    m_container.m_ledPanel.SetAllLEDColor(255,255,0);  //Yellow
-    m_container.m_drivetrain.InitFalcons();
-    m_container.m_drivetrain.Stop();
-    m_container.m_drivetrain.HardResetEncoders();
-    m_container.m_drivetrain.ZeroGyro(); 
-    m_container.m_drivetrain.ResetOdometry();
-    m_container.m_shooter.ShooterInit();
-
- }
+#include <frc/XboxController.h>
+#include <frc2/command/button/JoystickButton.h>
 
 
-void Robot::RobotPeriodic() 
-{
-  frc2::CommandScheduler::GetInstance().Run();
-  WriteToSmartDashboard();
-}
+#define LEFTDRIVE_FRONT_CAN_ID  1
+#define LEFTDRIVE_BACK_CAN_ID   2
+#define RIGHTDRIVE_FRONT_CAN_ID 3
+#define RIGHTDRIVE_BACK_CAN_ID  4 
 
+WPI_TalonFX m_leftMotorFront  { LEFTDRIVE_FRONT_CAN_ID };
+WPI_TalonFX m_leftMotorBack   { LEFTDRIVE_BACK_CAN_ID  };
+WPI_TalonFX m_rightMotorFront { RIGHTDRIVE_FRONT_CAN_ID };
+WPI_TalonFX m_rightMotorBack  { RIGHTDRIVE_BACK_CAN_ID };
 
-void Robot::DisabledInit()
- {
-    std::cout<<"Disabled Init"<<std::endl;
-    m_container.m_timer.Stop();
-    //m_container.m_topOperator.SetRumble(frc::GenericHID::kRightRumble,0);
-    //m_container.m_topOperator.SetRumble(frc::GenericHID::kLeftRumble,0);
- }
+frc::XboxController m_botDriver{0};
 
-void Robot::DisabledPeriodic() 
-{
+frc::DifferentialDrive m_differentialDrive{ m_leftMotorFront, m_rightMotorFront };
 
-  m_container.m_ledPanel.LedPanelPeriodic();
+class Robot : public frc::TimedRobot {
+  void RobotInit() override;
+  void RobotPeriodic() override;
+  void DisabledInit() override;
+  void DisabledPeriodic() override;
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+};
 
-}
+void Robot::RobotInit(){std::cout<<"pain train here we come"<<std::endl;}
+void Robot::RobotPeriodic(){}
+void Robot::DisabledInit(){}
+void Robot::DisabledPeriodic(){}
+void Robot::TeleopInit(){}
+void Robot::TeleopPeriodic(){}
 
-
-void Robot::AutonomousInit() 
-{
-  std::cout<<"Auto Init"<<std::endl;
-  m_autonomousCommand = m_container.GetAutonomousCommand();
-
-  if (m_autonomousCommand != nullptr) 
-  {
-    m_autonomousCommand->Schedule();
-  }
-
-
-  CheckAlliance();
-
-}
-
-void Robot::AutonomousPeriodic() 
-{
-
-}
-
-void Robot::TeleopInit()
-{
-  std::cout<<"Teleop Init"<<std::endl;
-  if (m_autonomousCommand != nullptr) 
-  {
-    m_autonomousCommand->Cancel();
-    m_autonomousCommand = nullptr;
-  }
-
-  CheckAlliance();
-
-  m_container.m_timer.Reset();
-  m_container.m_timer.Start();
-  //m_container.m_topOperator.SetRumble(frc::GenericHID::kRightRumble,1.0);
-  //m_container.m_topOperator.SetRumble(frc::GenericHID::kLeftRumble,1.0);
-}
-
-
-void Robot::TeleopPeriodic()
- {
-
- }
-
-
-
-void Robot::CheckAlliance( void )
-{
-
-  //Set Panel LEDs
-  if( m_ds.GetAlliance() == frc::DriverStation::kRed)
-  {
-      m_container.m_ledPanel.SetAllLEDColor(255, 0, 0);
-  }
-  else if(m_ds.GetAlliance() == frc::DriverStation::kBlue)
-  {
-      m_container.m_ledPanel.SetAllLEDColor(0,0,255);
-  }
-  else
-  {
-      m_container.m_ledPanel.SetAllLEDColor(255,255,0);
-  }
-
-
-
-}
-
-
-void Robot::WriteToSmartDashboard(void)
-{
-
-    //Timers
-    frc::SmartDashboard::PutNumber("FPGATime1", (double)m_container.m_timer.GetFPGATimestamp() );
-    frc::SmartDashboard::PutNumber("Timer",     (double)m_container.m_timer.Get() );
-    frc::SmartDashboard::PutNumber("MatchTime", (double)m_container.m_timer.GetMatchTime() );
-
-    //Joystick Test
-    frc::SmartDashboard::PutNumber("TopLeftXAxis",  (double)m_container.m_topOperator.GetRawAxis( GAMEPADMAP_AXIS_L_X ) ); 
-    frc::SmartDashboard::PutNumber("TopLeftYAxis",  (double)m_container.m_topOperator.GetRawAxis( GAMEPADMAP_AXIS_L_Y ) );
-    frc::SmartDashboard::PutNumber("TopRightXAxis", (double)m_container.m_topOperator.GetRawAxis( GAMEPADMAP_AXIS_R_X ) );
-    frc::SmartDashboard::PutNumber("TopRightYAxis", (double)m_container.m_topOperator.GetRawAxis( GAMEPADMAP_AXIS_R_Y ) );
-
-    // frc::SmartDashboard::PutBoolean("TopAButton", (bool)m_container.m_topOperator.GetRawButton( GAMEPADMAP_BUTTON_A ) );
-    // frc::SmartDashboard::PutBoolean("TopBButton", (bool)m_container.m_topOperator.GetRawButton( GAMEPADMAP_BUTTON_B ) );
-    // frc::SmartDashboard::PutBoolean("TopXButton", (bool)m_container.m_topOperator.GetRawButton( GAMEPADMAP_BUTTON_X ) );
-    // frc::SmartDashboard::PutBoolean("TopYButton", (bool)m_container.m_topOperator.GetRawButton( GAMEPADMAP_BUTTON_Y ) );
-
-
-    // frc::SmartDashboard::PutNumber("BotLeftXAxis",  (double)m_container.m_botDriver.GetRawAxis( GAMEPADMAP_AXIS_L_X ) ); 
-    // frc::SmartDashboard::PutNumber("BotLeftYAxis",  (double)m_container.m_botDriver.GetRawAxis( GAMEPADMAP_AXIS_L_Y ) );
-    // frc::SmartDashboard::PutNumber("BotRightXAxis", (double)m_container.m_botDriver.GetRawAxis( GAMEPADMAP_AXIS_R_X ) );
-    // frc::SmartDashboard::PutNumber("BotRightYAxis", (double)m_container.m_botDriver.GetRawAxis( GAMEPADMAP_AXIS_R_Y ) );
-
-    // frc::SmartDashboard::PutBoolean("BotAButton", (bool)m_container.m_botDriver.GetRawButton( GAMEPADMAP_BUTTON_A ) );
-    // frc::SmartDashboard::PutBoolean("BotBButton", (bool)m_container.m_botDriver.GetRawButton( GAMEPADMAP_BUTTON_B ) );
-    // frc::SmartDashboard::PutBoolean("BotXButton", (bool)m_container.m_botDriver.GetRawButton( GAMEPADMAP_BUTTON_X ) );
-    // frc::SmartDashboard::PutBoolean("BotYButton", (bool)m_container.m_botDriver.GetRawButton( GAMEPADMAP_BUTTON_Y ) );
-
-
-    //Nax-X
-	  frc::SmartDashboard::PutBoolean("navx_IsConn", m_container.m_drivetrain.IsGyroConnected() );
-	  frc::SmartDashboard::PutNumber("navx_Yaw",     m_container.m_drivetrain.GetGyroYaw()      );
-   	frc::SmartDashboard::PutNumber("navx_Angle",   m_container.m_drivetrain.GetGyroAngle()    );
-
-    //Odometry
-    frc::SmartDashboard::PutNumber("Curr_X",    m_container.m_drivetrain.GetOdometryX() );
-    frc::SmartDashboard::PutNumber("Curr_Y",    m_container.m_drivetrain.GetOdometryY() );
-    frc::SmartDashboard::PutNumber("Curr_Vel",  m_container.m_drivetrain.GetOdometryVel() );
-   	frc::SmartDashboard::PutNumber("Curr_Head", m_container.m_drivetrain.GetOdometryHeading() );
-
-    //Motors
-    frc::SmartDashboard::PutNumber("L_Motor",  m_container.m_drivetrain.GetLeftMotor()  );
-    frc::SmartDashboard::PutNumber("R_Motor",  m_container.m_drivetrain.GetRightMotor()  );
-
-    frc::SmartDashboard::PutNumber("LeftEnc",  m_container.m_drivetrain.GetLeftEncoder());
-//  frc::SmartDashboard::PutNumber("LeftEnc2", m_container.m_drivetrain.GetLeftEncoder2());
-    frc::SmartDashboard::PutNumber("RightEnc", m_container.m_drivetrain.GetRightEncoder());  
-//  frc::SmartDashboard::PutNumber("RightEnc2",m_container.m_drivetrain.GetRightEncoder2());
-
-    m_container.m_drivetrain.WriteFalconTemps();
-    m_container.m_shooter.WriteFalconTemps();
-
-}
-
-
-
-
-
-//*********************************************************
-
-void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
 int main() 
@@ -184,3 +50,34 @@ int main()
 }
 #endif
 
+
+
+// const double DEADBAND           = 0.05;
+//   	const double MAX_DRIVE_POWER    = 0.85;
+
+// 	double yL = m_ptrDriverXbox->GetRawAxis(GAMEPADMAP_AXIS_L_Y);
+// 	double xL = m_ptrDriverXbox->GetRawAxis(GAMEPADMAP_AXIS_L_X);
+// 	double yR = m_ptrDriverXbox->GetRawAxis(GAMEPADMAP_AXIS_R_Y);
+// 	double xR = m_ptrDriverXbox->GetRawAxis(GAMEPADMAP_AXIS_R_X); 
+
+// 	//double tL = m_driver_xbox->GetRawAxis(GAMEPADMAP_AXIS_L_TRIG);
+// 	//double tR = m_driver_xbox->GetRawAxis(GAMEPADMAP_AXIS_R_TRIG);
+
+// 	//Control DeadBand
+// 	if (fabs(yL)<= DEADBAND) yL = 0;
+// 	if (fabs(xL)<= DEADBAND) xL = 0;
+// 	if (fabs(yR)<= DEADBAND) yR = 0;
+// 	if (fabs(xR)<= DEADBAND) xR = 0;
+
+// 	//Subtract off deadband
+// 	if (fabs(yL)>= DEADBAND) yL +=  (yL>0)? -DEADBAND : +DEADBAND;
+// 	if (fabs(xL)>= DEADBAND) xL +=  (xL>0)? -DEADBAND : +DEADBAND;
+// 	if (fabs(yR)>= DEADBAND) yR +=  (yR>0)? -DEADBAND : +DEADBAND;
+// 	if (fabs(xR)>= DEADBAND) xR +=  (xR>0)? -DEADBAND : +DEADBAND;
+
+
+//     //Test Drive
+// 	yL *= MAX_DRIVE_POWER;	
+// 	yR *= MAX_DRIVE_POWER;	
+
+//   m_ptrDrivetrain->ArcadeDrive( -yL, xR );
