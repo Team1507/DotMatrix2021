463b486806df78f015bd527e95257b9d687e5632
Benjamin LaGreca <btl201@rit.edu>
2021-07-05 14:23:55 -0400

    Should be driveable? I have no idea

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -17,6 +17,9 @@
 #define RIGHTDRIVE_FRONT_CAN_ID 3
 #define RIGHTDRIVE_BACK_CAN_ID  4 
 
+#define MAX_DRIVE  0.85
+#define DEADZONE 0.05
+
 WPI_TalonFX m_leftMotorFront  { LEFTDRIVE_FRONT_CAN_ID };
 WPI_TalonFX m_leftMotorBack   { LEFTDRIVE_BACK_CAN_ID  };
 WPI_TalonFX m_rightMotorFront { RIGHTDRIVE_FRONT_CAN_ID };
@@ -30,12 +33,44 @@ frc::DifferentialDrive m_differentialDrive{ m_leftMotorFront, m_rightMotorFront 
 class Poop : public frc::TimedRobot {
   void RobotInit() override {std::cout<<"Pain train here we come"<<std::endl;}
   void RobotPeriodic() override{}
-  void DisabledInit() override{}
+  void DisabledInit() override
+  {
+    m_differentialDrive.ArcadeDrive( 0, 0);
+  }
   void DisabledPeriodic() override{}
   void TeleopInit() override{}
-  void TeleopPeriodic() override{}
-};
+  void TeleopPeriodic() override
+  {
+    // Deal with it B)
+    double yL = m_botDriver.GetRawAxis(1);
+    double xL = m_botDriver.GetRawAxis(0);
+    double yR = m_botDriver.GetRawAxis(5);
+    double xR = m_botDriver.GetRawAxis(4); 
 
+    //double tL = m_driver_xbox->GetRawAxis(GAMEPADMAP_AXIS_L_TRIG);
+    //double tR = m_driver_xbox->GetRawAxis(GAMEPADMAP_AXIS_R_TRIG);
+
+    //Control DEADZONE
+    if (fabs(yL)<= DEADZONE) yL = 0;
+    if (fabs(xL)<= DEADZONE) xL = 0;
+    if (fabs(yR)<= DEADZONE) yR = 0;
+    if (fabs(xR)<= DEADZONE) xR = 0;
+
+    //Subtract off DEADZONE
+    if (fabs(yL)>= DEADZONE) yL +=  (yL>0)? -DEADZONE : +DEADZONE;
+    if (fabs(xL)>= DEADZONE) xL +=  (xL>0)? -DEADZONE : +DEADZONE;
+    if (fabs(yR)>= DEADZONE) yR +=  (yR>0)? -DEADZONE : +DEADZONE;
+    if (fabs(xR)>= DEADZONE) xR +=  (xR>0)? -DEADZONE : +DEADZONE;
+
+
+      //Test Drive
+    yL *= MAX_DRIVE;	
+    yR *= MAX_DRIVE;	
+
+    m_differentialDrive.ArcadeDrive( -yL, xR );
+
+  }
+};
 
 
 #ifndef RUNNING_FRC_TESTS
@@ -45,34 +80,3 @@ int main()
 }
 #endif
 
-
-
-// const double DEADBAND           = 0.05;
-//   	const double MAX_DRIVE_POWER    = 0.85;
-
-// 	double yL = m_ptrDriverXbox->GetRawAxis(GAMEPADMAP_AXIS_L_Y);
-// 	double xL = m_ptrDriverXbox->GetRawAxis(GAMEPADMAP_AXIS_L_X);
-// 	double yR = m_ptrDriverXbox->GetRawAxis(GAMEPADMAP_AXIS_R_Y);
-// 	double xR = m_ptrDriverXbox->GetRawAxis(GAMEPADMAP_AXIS_R_X); 
-
-// 	//double tL = m_driver_xbox->GetRawAxis(GAMEPADMAP_AXIS_L_TRIG);
-// 	//double tR = m_driver_xbox->GetRawAxis(GAMEPADMAP_AXIS_R_TRIG);
-
-// 	//Control DeadBand
-// 	if (fabs(yL)<= DEADBAND) yL = 0;
-// 	if (fabs(xL)<= DEADBAND) xL = 0;
-// 	if (fabs(yR)<= DEADBAND) yR = 0;
-// 	if (fabs(xR)<= DEADBAND) xR = 0;
-
-// 	//Subtract off deadband
-// 	if (fabs(yL)>= DEADBAND) yL +=  (yL>0)? -DEADBAND : +DEADBAND;
-// 	if (fabs(xL)>= DEADBAND) xL +=  (xL>0)? -DEADBAND : +DEADBAND;
-// 	if (fabs(yR)>= DEADBAND) yR +=  (yR>0)? -DEADBAND : +DEADBAND;
-// 	if (fabs(xR)>= DEADBAND) xR +=  (xR>0)? -DEADBAND : +DEADBAND;
-
-
-//     //Test Drive
-// 	yL *= MAX_DRIVE_POWER;	
-// 	yR *= MAX_DRIVE_POWER;	
-
-//   m_ptrDrivetrain->ArcadeDrive( -yL, xR );
